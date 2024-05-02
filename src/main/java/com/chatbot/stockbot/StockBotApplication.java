f398e839c7c00d81546d6077932aeb4d3c5a076d
Voruganti <akhila.voruganti@accenture.com>
2024-05-02 11:05:55 +0530

    Added place holder for cloned repo

--- src/main/java/com/chatbot/stockbot/StockBotApplication.java
+++ src/main/java/com/chatbot/stockbot/StockBotApplication.java
@@ -3,7 +3,7 @@ package com.chatbot.stockbot;
 public class StockBotApplication {
 
 	public static void main(String[] args) {
-		String stockDataFilePath = "C:\\\\bi2i\\\\stock-bot\\\\stock_data.json";
+		String stockDataFilePath = "<cloned-repo-location>//stock_data.json";
 		StockBot bot = new StockBot(stockDataFilePath);
 
 	}
